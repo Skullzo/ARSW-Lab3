841d114558991c0278d43b01e5aa204e688beaaa
Alejandro Toro Daza <skullzo@outlook.com>
2021-02-04 09:15:56 -0500

    Parte 2

---
+++ Parte 2/src/main/java/edu/eci/arsw/blacklistvalidator/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.blacklistvalidator;
+import java.util.List;
+/**
+ *
+ * @author hcadavid
+ */
+public class Main {
+	public static void main(String a[]) throws InterruptedException{
+		long startTime = System.currentTimeMillis();
+        HostBlackListsValidator hblv=new HostBlackListsValidator();
+        List<Integer> blackListOcurrences=hblv.checkHost("200.24.34.55",100);
+        System.out.println("The host was found in the following blacklists:"+blackListOcurrences);  
+        long endTime = System.currentTimeMillis();
+        System.out.println("Tiempo de ejecución: " + (endTime - startTime) + " milisegundos.");
+    }  
+}
---
+++ Parte 2/src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.threads;
+/**
+ *
+ * @author hcadavid
+ */
+public class CountThreadsMain { 
+    public static void main(String a[]){
+        CountThread countThread1 = new CountThread(0,99);
+        CountThread countThread2 = new CountThread(99,199);
+        CountThread countThread3 = new CountThread(200,299);
+
+        Thread hilo1 = new Thread(countThread1);
+        Thread hilo2 = new Thread(countThread2);
+        Thread hilo3 = new Thread(countThread3);
+        /**
+        hilo1.start();
+        hilo2.start();
+        hilo3.start();
+        */
+        hilo1.run();
+        hilo2.run();
+        hilo3.run();
+    }  
+}
