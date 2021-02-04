ba2f5b2d1b7eeba01a023a8df7087b34ab8f44b9
Alejandro Toro Daza <skullzo@outlook.com>
2021-02-04 08:22:06 -0500

    Código Fuente

--- src/main/java/edu/eci/arst/concprg/prodcons/StartProduction.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/StartProduction.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package edu.eci.arst.concprg.prodcons;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
@@ -14,27 +13,17 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 public class StartProduction {
-    
-    
-    public static void main(String[] args) {
-        
-        Queue<Integer> queue=new LinkedBlockingQueue<>();
-        
-        
-        new Producer(queue,Long.MAX_VALUE).start();
-        
+    public static void main(String[] args) {       
+        Queue<Integer> queue=new LinkedBlockingQueue<>(); 
+        Producer p = new Producer(queue,Long.MAX_VALUE);
+        p.start(); 
         //let the producer create products for 5 seconds (stock).
         try {
             Thread.sleep(5000);
         } catch (InterruptedException ex) {
             Logger.getLogger(StartProduction.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
-        new Consumer(queue).start();
+        new Consumer(queue, p.getStockLimit()).start();
     }
-    
-
 }
