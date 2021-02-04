50131b0b0744d42c6e385e68a8ce74996793cffc
Alejandro Toro Daza <skullzo@outlook.com>
2021-02-04 09:15:40 -0500

    Parte 1 y 3

---
+++ Parte 1 y 3/src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arst.concprg.prodcons;
+import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author hcadavid
+ */
+public class Consumer extends Thread{   
+    private Queue<Integer> queue;
+    private int consumerStockLimit;   
+    public Consumer(Queue<Integer> queue, long stockLimit){
+        this.queue=queue; 
+        consumerStockLimit = (int) stockLimit;
+    }
+    @Override
+    public void run() {
+//		optimalConsumption();
+		lowConsumption();
+//		originalConsumption();
+    }
+    public void originalConsumption() {
+		while (true) {
+
+			if (queue.size() > 0) {
+				int elem = queue.poll();
+				System.out.println("Consumer consumes " + elem);
+			}
+
+		}
+	}
+    public void optimalConsumption() {
+		synchronized (queue) {
+			while (true) {
+				if (queue.size() > 0) {
+					int elem = queue.poll();
+					System.out.println("Consumer consumes " + elem);
+					queue.notify();
+				} else {
+					try {
+						queue.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+    public void lowConsumption() {
+		while (true) {
+			if (queue.size() > 0) {
+				int elem = queue.poll();
+				System.out.println("Consumer consumes " + elem);				
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException ex) {
+				Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+			}
+
+		}
+	}
+}
---
+++ Parte 1 y 3/src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arst.concprg.prodcons;
+import java.util.Queue;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author hcadavid
+ */
+public class Producer extends Thread {
+    private Queue<Integer> queue = null;
+    private int dataSeed = 0;
+    private Random rand=null;
+    private final long stockLimit;
+    public Producer(Queue<Integer> queue,long stockLimit) {
+        this.queue = queue;
+        rand = new Random(System.currentTimeMillis());
+        this.stockLimit=stockLimit;
+    }
+    public long getStockLimit() {
+		return stockLimit;
+	}
+    @Override
+    public void run() {
+//		optimalProduction();
+		fastProduction();
+//		originalProduction();
+    }
+    public void originalProduction() {
+		while (true) {
+			dataSeed = dataSeed + rand.nextInt(100);
+			System.out.println("Producer added " + dataSeed);
+			queue.add(dataSeed);
+
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException ex) {
+				Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+	}
+    public void optimalProduction() {
+		synchronized (queue) {
+			while (true) {
+				if (queue.size() == 0) {
+					dataSeed = dataSeed + rand.nextInt(100);
+					System.out.println("Producer added " + dataSeed);
+					queue.add(dataSeed);
+					queue.notify();
+				} else {
+					try {
+						queue.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException ex) {
+					Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+				}
+			}
+		}
+	}
+    public void fastProduction() {
+		while (true) {
+			if (queue.size() < 10) {
+				dataSeed = dataSeed + rand.nextInt(100);
+				System.out.println("Producer added " + dataSeed);
+				queue.add(dataSeed);				
+			}
+		}
+	}
+}
